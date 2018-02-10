49bea8bf72af3072f1ecd8fdd1491c841077ef8f
Zeeshan Khan <zkhan1093@gmail.com>
2018-02-10 18:34:29 +0530

    using jsonify

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return json.dumps(dict(content='Hello World'))
+	return jsonify(dict(content='Hello World'))
 
 if __name__=='__main__':
 	app.run(debug=True)
