e437b13a99482ef1c77ce6fc018b418cec331646
Zeeshan Khan <zkhan1093@gmail.com>
2018-02-27 02:05:18 +0530

    login page improved ui and added vue app

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,19 +1,48 @@
-var showloading = function(){
-    $('#login-btn').addClass('loading disabled');
-    $('#error-message').addClass('hidden');
-};
-var hideLoading = function(){
-    $('#login-btn').removeClass('loading disabled');
-    $('#error-message').removeClass('hidden');
-};
-var saveToken = function(result){
-        console.log(result);
-        var values = {expires: 1}
-        Cookies.set('auth_token',result.auth_token, values);
-        Cookies.set('is_admin',result.is_admin, values);
-        window.location = "/attendance";
-};
-var errorgettingToken = function(result){
-    $('#error-message').html(result.responseJSON.message);
-    hideLoading();
-}
+var app = new Vue({
+    el: '#logincontainer',
+    data: {
+        error:'',
+        loading: false,
+    },
+    methods:{
+        getToken( event ) {
+            event.preventDefault();
+            this.loading=true;
+            var postData = this.getJsonFromForm($('#loginForm input'))
+            var vm = this;
+            var url = '/api/get_token';
+
+            vm.$http.post(url,postData)
+            .then((response) => {
+                console.log(response);
+                console.log(response.body);
+                var values = {expires: 1}
+                Cookies.set('auth_token',response.body.auth_token, values);
+                Cookies.set('is_admin',response.body.is_admin, values);
+                this.loading = true;
+                window.location = "/attendance";
+            },
+            (error) => {
+                console.log(error);
+                var message = 'Something bad happened';
+                if(error.body)
+                    message = error.body.message;
+                else if (error.statusText)
+                    message = error.statusText
+                vm.error = message
+                vm.loading = false
+            });
+        },
+        showForgotDialog(){
+            $('#forgotModal').modal('show');
+        },
+        getJsonFromForm: function(formInputArr){
+            data = {}
+            $(formInputArr).each(function(index, input){
+                data[input.name] = input.value;
+                input.value = '';
+            });
+            return data;
+        },
+    }
+});
