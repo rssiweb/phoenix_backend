76e73b306befd76e0dfc140e476567bedc7f2cab
Zeeshan Khan <zkhan1093@gmail.com>
2018-07-02 03:12:37 +0530

    worker fix

--- worker.py
+++ worker.py
@@ -1,10 +1,13 @@
-from config import REDISTOGO_URL
-import redis
 from rq import Worker, Queue, Connection
+import redis
+import os
+
 
 listen = ['high', 'default', 'low']
 
-conn = redis.from_url(REDISTOGO_URL)
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+
+conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
