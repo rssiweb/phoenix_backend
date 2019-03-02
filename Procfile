4ccec213abe825d7647659131d863ab105ff831e
Zeeshan Khan <zkhan1093@gmail.com>
2019-03-03 00:53:30 +0530

    worker.py with app

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: flask db upgrade; gunicorn run:app
-worker: rq worker -u $REDISTOGO_URL high default low
+worker: python worker.py
---
+++ worker.py
@@ -0,0 +1,15 @@
+import os
+import redis
+from rq import Worker, Queue, Connection
+from app import create_app
+
+app = create_app()
+
+listen = ['high', 'default', 'low']
+
+conn = redis.from_url(os.getenv('REDISTOGO_URL'))
+
+if __name__ == '__main__':
+    with Connection(conn):
+        worker = Worker(map(Queue, listen))
+        worker.work()
